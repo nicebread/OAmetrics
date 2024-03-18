de4fa26e5d0f2bdaa1895d16c0c1f7b0792cb1d5
Felix.S <felix@nicebread.de>
2024-03-18 06:31:06 +0100

    Make all CHECKS happy

---
+++ R/imports.R
@@ -0,0 +1,6 @@
+#' @importFrom utils read.csv packageVersion
+
+# add globalVariables to appease R check
+globalVariables(c("au_id", "index", "institution_country_code", "level", "n_coauthorships", "so", "."))
+
+NULL
